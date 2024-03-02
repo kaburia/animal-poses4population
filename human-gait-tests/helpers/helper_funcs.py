1542d579a2edc0fc4fb86323ac1ad212bf62b655
kaburia <kaburiaaustin1@gmail.com>
2024-03-02 17:10:49 +0300

    Fix video loading error

--- human-gait-tests/helpers/helper_funcs.py
+++ human-gait-tests/helpers/helper_funcs.py
@@ -22,20 +22,27 @@ def mediapipe_pose_graph(landmarks: dict, pose_graph: dict, draw: bool = False):
     # Initialize a new graph
     G = nx.Graph()
 
-    node_positions = {k: (v['x'], -v['y']) for k, v in landmarks.items()}
+    node_positions = {k: (0, 0) if v['visibility'] == 0 else (v['x'], -v['y']) for k, v in landmarks.items()}
 
     # Add nodes with positions and existence probability
     for node, coord_prob in landmarks.items():
         # x,y is zero if the visibility is zero
-        x, y = coord_prob['x'], coord_prob['y']
+        if coord_prob['visibility'] == 0:
+            x, y = 0, 0
+        else:
+            x, y = coord_prob['x'], coord_prob['y']
         G.add_node(node, pos=(x, y), probability=coord_prob['visibility'])
 
     # Add edges with weights
     for node, neighbors in pose_graph.items():
         for neighbor in neighbors:
+            # check if the nodes are present
+            if landmarks[neighbor]['visibility'] == 0 or landmarks[node]['visibility'] == 0:
+                    continue
             x1, y1 = landmarks[node]['x'], landmarks[node]['y']
             x2, y2 = landmarks[neighbor]['x'], landmarks[neighbor]['y']
             weight = ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+            
             G.add_edge(node, neighbor, weight=weight)
 
     # Draw the graph
@@ -104,13 +111,13 @@ def display_frame_at_frame_count(frame_count, file_no):
     video_file = os.path.join(os.getcwd(), 'video', f'video_{file_no}.avi')
 
     # Define the framerate
-    framerate = 30
+    # framerate = 60
 
     cap = cv2.VideoCapture(video_file)
 
     # Set the frame position to the desired point
-    frame_position = int(frame_count / framerate * cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_position)
+    # frame_position = int(frame_count / framerate * cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_count)
 
     # Read the frame at the desired point
     success, frame = cap.read()
